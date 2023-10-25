82eb2cc952a087bf556f5fdfff5df21108a156e5
KonstantinSerebryakov <KonstantinSerebryakov2000@gmail.com>
2023-10-25 14:14:25 +0300

    tmp commit

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,8 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16'
       },
 
-      vueRouterMode: 'history', // available values: 'hash', 'history'
+      // vueRouterMode: 'history', // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
